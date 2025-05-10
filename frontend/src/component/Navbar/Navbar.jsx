64c774a2b3005a5f702b2ce96d09d66faa510734
Arunkumar262004 <arunkumar957877@gmail.com>
2025-05-10 21:34:47 +0530

    Refactor Navbar component for improved code readability and consistency

--- frontend/src/component/Navbar/Navbar.jsx
+++ frontend/src/component/Navbar/Navbar.jsx
@@ -1,16 +1,16 @@
 import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
-import {Link, useNavigate} from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { StoreContext } from '../../context/Storecontext'
 
-const Navbar = ({setShowLogin}) => {
+const Navbar = ({ setShowLogin }) => {
 
   const [menu, Setmenu] = useState("")
 
-  const{get_total_Cart_amount,token,setToken} = useContext(StoreContext)
+  const { get_total_Cart_amount, token, setToken } = useContext(StoreContext)
 
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const logout = () => {
     localStorage.removeItem("token")
@@ -30,18 +30,18 @@ const navigate = useNavigate();
         <img src={assets.search_icon} alt="" />
         <div className='navbar-search-icon'>
           <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
-          <div className={get_total_Cart_amount() === 0?"":"dot"}></div>
+          <div className={get_total_Cart_amount() === 0 ? "" : "dot"}></div>
         </div>
-        {!token? <button onClick={()=>{setShowLogin(true)}}>Sign Up</button> :
-        <div className="navbar-profile">
-          <img src={assets.profile_icon} alt="" />
-          <ul className="nav-profile-dropdown">
-            <li><img src={assets.bag_icon} alt="" />Orders</li>
-            <hr/>
-            <li onClick={logout}><img src={assets.logout_icon} alt="" />Logout</li>
-          </ul>
-        </div>    }
-        
+        {!token ? <button onClick={() => { setShowLogin(true) }}>Sign Up</button> :
+          <div className="navbar-profile">
+            <img src={assets.profile_icon} alt="" />
+            <ul className="nav-profile-dropdown">
+              <li><img src={assets.bag_icon} alt="" />Orders</li>
+              <hr />
+              <li onClick={logout}><img src={assets.logout_icon} alt="" />Logout</li>
+            </ul>
+          </div>}
+
       </div>
     </div>
   )
